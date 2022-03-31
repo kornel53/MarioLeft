cb17abb9b8a728e6ac0864c89a0a12d8ecb02f06
Korneliusz Krawczyk <korneliusz_krawczyk@o2.pl>
2022-03-31 14:21:03 +0200

    Fix multi-jump bug

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,13 @@ public class Player : MonoBehaviour
     public float Speed = 1.0f;
     public float JumpForce = 1.0f;
 
+    protected bool CanJump = true;
+
     void Update()
     {
+        var hit = Physics2D.Raycast(GetComponent<Transform>().position, Vector2.down, 0.08f);
+        CanJump = hit.collider != null ? true : false;
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             GetComponent<Transform>().Translate(new Vector2(-1 * Speed * Time.deltaTime, 0));
